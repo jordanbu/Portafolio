b9ef6f5db2fc3a61c251a78afac90bf2ac25f86d
jordanbu <burgosjordan194@gmail.com>
2025-05-31 21:10:16 -0400

    Cambios realizados

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -46,28 +46,31 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-        <div className="navbar_container">
-          <Link to="/" className="navbar_container_logo">
-            <FaReact size={30} />
-          </Link>
-        </div>
-        <ul className={`navbar_container_menu ${toggleIcon ? "active" : ""}`}>
-          {data.map((item) => (
-            <li key={item.label} className="navbar_container_menu_item">
-              <Link
-                className="navbar_container_menu_item_links"
-                to={item.to}
-                onClick={handleLinkClick} // Close menu on link click
-              >
-                {item.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="nav_icon" onClick={handleToggleIcon}>
-          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
-        </div>
-      </nav>
+  <div className="navbar_container">
+    <Link to="/" className="navbar_container_logo">
+      <FaReact size={30} />
+    </Link>
+
+    <div className="nav_icon" onClick={handleToggleIcon}>
+      {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+    </div>
+  </div>
+
+  <ul className={`navbar_container_menu ${toggleIcon ? "active" : ""}`}>
+    {data.map((item) => (
+      <li key={item.label} className="navbar_container_menu_item">
+        <Link
+          className="navbar_container_menu_item_links"
+          to={item.to}
+          onClick={handleLinkClick}
+        >
+          {item.label}
+        </Link>
+      </li>
+    ))}
+  </ul>
+</nav>
+
     </div>
   );
 };
