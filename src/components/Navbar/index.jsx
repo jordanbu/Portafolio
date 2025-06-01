de8ee3d660ee9855428417668c901071ff429ee9
jordanbu <burgosjordan194@gmail.com>
2025-05-31 21:15:11 -0400

    Cambios realizados

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,47 +1,74 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { FaBars, FaReact } from "react-icons/fa";
 import { HiX } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import "./styles.scss";
+
+const data = [
+  {
+    label: "INICIO",
+    to: "/",
+  },
+  {
+    label: "YO",
+    to: "/about",
+  },
+  {
+    label: "HABILIDADES",
+    to: "/skills",
+  },
+  {
+    label: "RESUMEN",
+    to: "/resume",
+  },
+  {
+    label: "PORTAFOLIO",
+    to: "/portfolio",
+  },
+  {
+    label: "CONTACTO",
+    to: "/contact",
+  },
+];
 
 const Navbar = () => {
   const [toggleIcon, setToggleIcon] = useState(false);
 
-  const handleToggleIcon = () => setToggleIcon(!toggleIcon);
-  const handleLinkClick = () => setToggleIcon(false);
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
 
-  const data = [
-    { label: "Inicio", to: "/" },
-    { label: "Sobre mí", to: "/about" },
-    { label: "Proyectos", to: "/projects" },
-    { label: "Contacto", to: "/contact" }
-  ];
+  // Close the menu when a link is clicked
+  const handleLinkClick = () => {
+    setToggleIcon(false);
+  };
 
   return (
-    <nav className="navbar">
-      <div className="navbar_container">
-        <Link to="/" className="navbar_container_logo">
-          <FaReact size={30} />
-        </Link>
-
+    <div>
+      <nav className="navbar">
+        <div className="navbar_container">
+          <Link to="/" className="navbar_container_logo">
+            <FaReact size={30} />
+          </Link>
+        </div>
+        <ul className={`navbar_container_menu ${toggleIcon ? "active" : ""}`}>
+          {data.map((item) => (
+            <li key={item.label} className="navbar_container_menu_item">
+              <Link
+                className="navbar_container_menu_item_links"
+                to={item.to}
+                onClick={handleLinkClick} // Close menu on link click
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
         <div className="nav_icon" onClick={handleToggleIcon}>
           {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
         </div>
-      </div>
-
-      <ul className={`navbar_container_menu ${toggleIcon ? "active" : ""}`}>
-        {data.map((item) => (
-          <li key={item.label} className="navbar_container_menu_item">
-            <Link
-              className="navbar_container_menu_item_links"
-              to={item.to}
-              onClick={handleLinkClick}
-            >
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
