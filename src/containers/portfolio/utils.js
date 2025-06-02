2d326cc29a3e99c7f152493699cdd0660707437b
jordanbu <burgosjordan194@gmail.com>
2025-06-02 15:30:46 -0400

    Cambios realizados

--- src/containers/portfolio/utils.js
+++ src/containers/portfolio/utils.js
@@ -6,15 +6,14 @@ export const portfolioData = [
     link: "https://myecommerce.example.com",
   },
   {
-    title: "Weather Dashboard",
-    description: "Clima actual y pronóstico usando OpenWeatherMap API.",
-    image: "/images/weatherapp.png",
-    link: "https://weatherdashboard.example.com",
-  },
-  {
     title: "App de Geolocalización con React y Google Maps",
     description:
       "Aplicación web que permite ubicar al usuario y mostrar puntos de interés en el mapa usando la API de Google Maps.",
     video: "https://www.youtube.com/embed/UKmMeRpzmD8",
+  },
+  {
+    title: "Mi Portafolio Animado en React Native",
+    description: "Portafolio interactivo con React Native y efecto Particles.js, diseño moderno y responsive.",
+    video: "https://www.youtube.com/embed/FCnmUb93OTA",
   },
 ];
